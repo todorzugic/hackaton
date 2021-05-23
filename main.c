8ed03363887d0a902f05587521e2f776d90bddc1
Todor Zugic <todorzugic@localhost.localdomain>
2021-05-23 12:52:56 +0200

    maybe

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int  error_handling(char **argv, int argc)
+{
+    if (argc != 1)
+        return (84);
+    return (0);
+}
+
+int firstBadVersion(int n)
+{
+    int t = 0;
+
+    while (isBadVersion(t) == true) {
+        t++;
+    }
+    return (t);
+}
+
+int main(char **argv, int argc)
+{
+    int t;
+
+    if (error_handling(argv, argc) == 84)
+        return (84);
+    t = firstBadVersion(argv[1]);
+    printf("Output: %d\n", t);
+    return (0);
+}
