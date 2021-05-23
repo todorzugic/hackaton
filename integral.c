fa661efac28358679a1539ef659c674a40a87d03
Todor Zugic <todorzugic@localhost.localdomain>
2021-05-23 17:19:51 +0200

    maybe?

--- integral.c
+++ integral.c
@@ -4,26 +4,29 @@
 #include <string.h>
 #include <stdbool.h>
 
-int integralReplacement(int t)
+int chose (int first, int second)
 {
-    int counter = 0;
+    return (first <= second ? first : second);
+}
 
-    while (t != 1) {
+int integerReplacement(int t)
+{
+    int counter = 1;
+
+    if (t == 1) {
+        return 0;
+    } else {
         if (t % 2 == 0) {
-            t = t / 2;
-            counter++;
-        }
-        else {
-            t = t + 1;
-            counter++;
+            counter += integerReplacement(t / 2);
+        } else {
+            counter += chose(integerReplacement(t - 1), integerReplacement(t + 1));
         }
     }
-    printf("%d\n", counter);
     return (counter);
 }
 
 int main (int argc, char **argv)
 {
-    integralReplacement(atoi(argv[1]));
-    return (0);
+    int t = integerReplacement(atoi(argv[1]));
+    return (t);
 }
