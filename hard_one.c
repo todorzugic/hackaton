a0d676ca67cbfa3ec7c79036e79ba2814eb4421a
Todor Zugic <todorzugic@localhost.localdomain>
2021-05-29 19:29:40 +0200

    shiet

---
+++ hard_one.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isPrintable(int** targetGrid, int targetGridSize, int* targetGridColSize) 
+{
+    return (true);
+}
